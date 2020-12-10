eadb13cc23e75314b34e51c5473d3c7f2f1347e1
yws <yws528819@163.com>
2020-12-10 08:09:34 +0800

    追加注释

--- MyBatis_01_HelloWorld/src/com/yws/test/MyBatisTest.java
+++ MyBatis_01_HelloWorld/src/com/yws/test/MyBatisTest.java
@@ -83,12 +83,14 @@ class MyBatisTest {
 	
 	/**
 	 * 1.获取sqlSessionFactory对象：
-	 * 		解析文件的每一个信息保存在Configuration中，返回包含Configuration的DefaultSqlSession
+	 * 		解析文件的每一个信息保存在Configuration中，返回包含Configuration的DefaultSqlSessionFactory
 	 * 		注意：【MappedStatement】：代表一个增删改查的详细信息
 	 * 2.获取sqlSession对象
-	 * 	
-	 * 
+	 * 		返回一个DefaultSqlSession对象，包含Executor和Configuration
+	 * 		这一步会创建Executor对象；
 	 * 3.获取接口的代理对象（MapperProxy）
+	 * 		getMapper，使用MapperProxyFactory创建了一个MapperProxy的代理对象
+	 * 		代理对象里面包含了，DefaultSqlSession(Executor)
 	 * 4.执行增删改查方法
 	 * 
 	 * @throws Exception

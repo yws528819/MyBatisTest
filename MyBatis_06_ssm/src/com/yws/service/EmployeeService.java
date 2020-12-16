c73a39e361ead952dd3a3d3a283fc26f4d693229
yws <yws528819@163.com>
2020-12-16 13:52:08 +0800

    批量处理，spring工程写法

--- MyBatis_06_ssm/src/com/yws/service/EmployeeService.java
+++ MyBatis_06_ssm/src/com/yws/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.yws.service;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,14 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeMapper employeeMapper;
 	
+	@Autowired
+	private SqlSession sqlSession;
+	
 	public List<Employee> getEmps() {
-		return employeeMapper.getEmps();
+		EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+		return mapper.getEmps();
+		
+		
+		//return employeeMapper.getEmps();
 	}
 }
